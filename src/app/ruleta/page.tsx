4d33589797892fd595fb2254720941d1b24b0cab
lexrojas <legalcr.rojas@gmail.com>
2025-08-19 18:46:06 -0600

    denuevo

--- src/app/ruleta/page.tsx
+++ src/app/ruleta/page.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 import { guardarClientePromocion, verificarCliente } from "./actions";
 
 
-export default function HomePage() {
+function RuletaPage() {
 
     const searcParams = useSearchParams()
 
@@ -88,62 +88,73 @@ export default function HomePage() {
     }
 
     return (
-        <Suspense fallback={<div>Cargando...</div>}>
-            <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 md:p-24 bg-gray-100">
-                <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-xl">
-                    <h1 className="text-2xl font-bold text-center text-red-400">Felicidades, puedes participar en nuestra rifa!</h1>
-                    <div className="flex flex-col items-center justify-center bg-white">
-                        {/* Contenedor principal */}
-                        <div className="relative w-64 h-64 flex items-center justify-center">
-                            <Image src="/frameRulet.svg" alt="Frame Ruleta" fill className="object-contain z-0" priority />
 
-                            {/* Puntero superior */}
-                            <div className="absolute -top-0 z-20">
-                                <div className="w-0 h-0" style={{ borderLeft: "12px solid transparent", borderRight: "12px solid transparent", borderTop: "24px solid #111" }} />
-                            </div>
+        <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 md:p-24 bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-xl">
+                <h1 className="text-2xl font-bold text-center text-red-400">Felicidades, puedes participar en nuestra rifa!</h1>
+                <div className="flex flex-col items-center justify-center bg-white">
+                    {/* Contenedor principal */}
+                    <div className="relative w-64 h-64 flex items-center justify-center">
+                        <Image src="/frameRulet.svg" alt="Frame Ruleta" fill className="object-contain z-0" priority />
 
-                            {/* Ruleta */}
-                            <div
-                                className="absolute flex items-center justify-center z-10"
-                                style={{
-                                    width: "13rem", // ≈ w-52
-                                    height: "13rem",
-                                    transform: `rotate(${angle}deg)`,
-                                    transition: isSpinning ? "transform 2s cubic-bezier(0.22, 1, 0.36, 1)" : "none",
-                                }}
-                                onTransitionEnd={handleTransitionEnd}
-                            >
-                                <Image src="/rulet.svg" alt="Ruleta" fill className="object-contain" priority />
-                            </div>
+                        {/* Puntero superior */}
+                        <div className="absolute -top-0 z-20">
+                            <div className="w-0 h-0" style={{ borderLeft: "12px solid transparent", borderRight: "12px solid transparent", borderTop: "24px solid #111" }} />
                         </div>
 
-                        {/* Botón */}
-                        <button
-                            onClick={handleSpin}
-                            disabled={isSpinning || played}
-                            className={`mt-10 px-6 py-3 text-white font-semibold rounded-lg shadow-md transition
-              ${isSpinning ? "bg-gray-500 cursor-not-allowed" : "bg-rose-400 hover:bg-red-800"}`}
-                            hidden={participated}
+                        {/* Ruleta */}
+                        <div
+                            className="absolute flex items-center justify-center z-10"
+                            style={{
+                                width: "13rem", // ≈ w-52
+                                height: "13rem",
+                                transform: `rotate(${angle}deg)`,
+                                transition: isSpinning ? "transform 2s cubic-bezier(0.22, 1, 0.36, 1)" : "none",
+                            }}
+                            onTransitionEnd={handleTransitionEnd}
                         >
-                            GIRAR
-                        </button>
-
-                        {/* Resultado */}
-                        {finalLabel ? (
-                            <p className="my-6 text-sm text-gray-800">
-                                <span className="text-3xl font-bold animate-pulse text-rose-400">{finalLabel}</span>
-                            </p>
-                        ) : (
-                            <p className="mt-4 text-sm text-gray-800">
-                                <span className="font-semibold">Averigua tu suerte !</span>
-                            </p>
-                        )}
-                        {participated && (
-                            <p className="mt-4 text-sm text-gray-800"> Ya usted participó anteriormente, seguro que has ganado algo, reclama tu premio por favor!</p>
-                        )}
+                            <Image src="/rulet.svg" alt="Ruleta" fill className="object-contain" priority />
+                        </div>
                     </div>
+
+                    {/* Botón */}
+                    <button
+                        onClick={handleSpin}
+                        disabled={isSpinning || played}
+                        className={`mt-10 px-6 py-3 text-white font-semibold rounded-lg shadow-md transition
+              ${isSpinning ? "bg-gray-500 cursor-not-allowed" : "bg-rose-400 hover:bg-red-800"}`}
+                        hidden={participated}
+                    >
+                        GIRAR
+                    </button>
+
+                    {/* Resultado */}
+                    {finalLabel ? (
+                        <p className="my-6 text-sm text-gray-800">
+                            <span className="text-3xl font-bold animate-pulse text-rose-400">{finalLabel}</span>
+                        </p>
+                    ) : (
+                        <p className="mt-4 text-sm text-gray-800">
+                            <span className="font-semibold">Averigua tu suerte !</span>
+                        </p>
+                    )}
+                    {participated && (
+                        <p className="mt-4 text-sm text-gray-800"> Ya usted participó anteriormente, seguro que has ganado algo, reclama tu premio por favor!</p>
+                    )}
                 </div>
-            </main>
-        </Suspense>
+            </div>
+        </main>
+
     );
+}
+
+
+export default function home() {
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}  >
+            <RuletaPage />
+        </Suspense>
+    )
+
 }
